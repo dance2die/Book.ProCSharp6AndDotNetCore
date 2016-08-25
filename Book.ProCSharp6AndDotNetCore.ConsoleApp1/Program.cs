fb5ed1db80a18f174b65a6463234f5766d2c1042
dance2die <dontbothermewithspam@gmail.com>
2016-08-25 16:10:43 -0400

    Created Book.ProCSharp6AndDotNetCore.ConsoleApp1

---
+++ Book.ProCSharp6AndDotNetCore.ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Book.ProCSharp6AndDotNetCore.ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
