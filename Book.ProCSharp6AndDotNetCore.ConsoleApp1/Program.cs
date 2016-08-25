2d1e706bbf5d41b8c7fbe67979fe845c4812760b
dance2die <dontbothermewithspam@gmail.com>
2016-08-25 16:19:32 -0400

    Task delay test using Parallel.For

--- Book.ProCSharp6AndDotNetCore.ConsoleApp1/Program.cs
+++ Book.ProCSharp6AndDotNetCore.ConsoleApp1/Program.cs
@@ -1,14 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+
+using static System.Console;
 
 namespace Book.ProCSharp6AndDotNetCore.ConsoleApp1
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-        }
-    }
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var result =
+				Parallel.For(0, 10, i =>
+				{
+					Log($"S {i}");
+					Task.Delay(10).Wait();
+					Log($"E {i}");
+				});
+
+			Console.WriteLine($"Is Completed: {result.IsCompleted}");
+		}
+
+		public static void Log(string prefix)
+		{
+			WriteLine($"{prefix}, task: {Task.CurrentId}, " +
+				$"thread: {Thread.CurrentThread.ManagedThreadId}");
+		}
+	}
 }
